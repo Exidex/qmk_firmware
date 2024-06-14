be7b992afa115065226821dd40809e6e1c9e1748
Exidex <16986685+Exidex@users.noreply.github.com>
2024-06-14 18:36:50 +0200

    Replace tap dance with multiple momentary layers

--- keyboards/beekeeb/piantor_pro/keymaps/default/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/default/keymap.c
@@ -16,68 +16,33 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     NULL // terminate the array
 };
 
-typedef enum {
-    TD_NONE,
-    TD_UNKNOWN,
-    TD_SINGLE_TAP,
-    TD_SINGLE_HOLD,
-    TD_DOUBLE_TAP,
-    TD_DOUBLE_HOLD
-} td_state_t;
-
-typedef struct {
-    bool is_press_action;
-    td_state_t state;
-} td_tap_t;
-
-enum {
-    NUM_FN_TAP_DANCE,
-};
-
-void num_fn_finished(tap_dance_state_t *state, void *user_data);
-void num_fn_reset(tap_dance_state_t *state, void *user_data);
-
-tap_dance_action_t tap_dance_actions[] = {
-    [NUM_FN_TAP_DANCE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, num_fn_finished, num_fn_reset)
-    // TODO TAP DANCE ON SYMBOL LAYER TO HELP ONE HAND OPERATIONS
-};
-
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case QK_TAP_DANCE ... QK_TAP_DANCE_MAX:
-            return 275;
-        default:
-            return TAPPING_TERM;
-    }
-}
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEFAULT_LAYER] = LAYOUT_split_3x6_3(
         KC_TAB,      KC_Q,         KC_W,          KC_E,         KC_R,            KC_T,                               KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,            KC_RALT,
         KC_ESCAPE,   LGUI_T(KC_A), LALT_T(KC_S),  LCTL_T(KC_D), LSFT_T(KC_F),    KC_G,                               KC_H,    RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_INS,
         KC_LSFT,     KC_Z,         KC_X,          KC_C,         KC_V,            KC_B,                               KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RSFT,
-                                MO(SYMBOL_LAYER),   LT(NAV_LAYER, KC_LEFT_GUI),   BSPC_OR_DEL,                 KC_ENT,   KC_SPC, TD(NUM_FN_TAP_DANCE)
+                                                MO(SYMBOL_LAYER),   MO(NAV_LAYER),   BSPC_OR_DEL,                 KC_SPC, KC_ENT, MO(NUM_LAYER)
     ),
 
     [NAV_LAYER] = LAYOUT_split_3x6_3(
         KC_NO,  KC_NO,      C(S(KC_TAB)),    KC_UP,      C(KC_TAB),   KC_PGUP,                               KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,  KC_HOME,    KC_LEFT,         KC_DOWN,    KC_RIGHT,    KC_END,                                KC_NO,    KC_RSFT,    KC_RCTL,  KC_LALT,  KC_RGUI,  KC_NO,
         KC_NO,  KC_NO,      S(KC_TAB),       KC_APP,     KC_TAB,      KC_PGDN,                               KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,    KC_NO,
-                                                                 KC_NO, KC_NO, KC_NO,                KC_NO, KC_NO, KC_NO
+                                                                 KC_NO, KC_NO, KC_NO,                KC_NO, KC_LEFT_GUI, KC_NO
     ),
 
     [SYMBOL_LAYER] = LAYOUT_split_3x6_3(
         KC_NO,     KC_EXLM,    KC_AT,     KC_HASH,   KC_DLR,    KC_PERC,                         KC_CIRC,   KC_AMPR,  KC_ASTR,    KC_UNDS,    KC_PLUS,   KC_NO,
         KC_NO,     KC_GRV,     KC_TILDE,  KC_LBRC,   KC_RBRC,   KC_BSLS,                         KC_QUOT,   KC_LPRN,  KC_RPRN,    KC_EQL,     KC_MINUS,  KC_NO,
         KC_NO,     KC_NO,      KC_NO,     KC_LT,     KC_GT,     KC_PIPE,                         KC_DQUO,   KC_LCBR,  KC_RCBR,    KC_NO,      KC_NO,     KC_NO,
-                                                           KC_NO, KC_NO, KC_NO,           KC_NO, KC_NO, KC_NO
+                                                           KC_NO, KC_NO, KC_NO,           KC_NO, KC_NO, MO(FN_LAYER)
     ),
 
     [NUM_LAYER] = LAYOUT_split_3x6_3(
         KC_AUDIO_MUTE,        KC_NO,    KC_NO,    KC_NO,    KC_MEDIA_NEXT_TRACK,    KC_AUDIO_VOL_UP,                              KC_NO,    KC_7,    KC_8,    KC_9,    KC_NO,    KC_NO,
         KC_MEDIA_STOP,        KC_NO,    KC_NO,    KC_NO,    KC_MEDIA_PREV_TRACK,    KC_AUDIO_VOL_DOWN,                            KC_0,     KC_4,    KC_5,    KC_6,    KC_NO,    KC_NO,
         KC_MEDIA_PLAY_PAUSE,  KC_NO,    KC_NO,    KC_NO,    KC_NO,                  KC_NO,                                        KC_NO,    KC_1,    KC_2,    KC_3,    KC_NO,    KC_NO,
-                                                                                        KC_NO, KC_NO, KC_NO,               KC_NO, KC_NO, KC_NO
+                                                                                   MO(FN_LAYER), KC_NO, KC_NO,               KC_NO, KC_NO, KC_NO
     ),
 
     [FN_LAYER] = LAYOUT_split_3x6_3(
@@ -88,56 +53,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 };
-
-
-td_state_t cur_dance(tap_dance_state_t *state) {
-    if (state->count == 1) {
-        if (!state->pressed) return TD_SINGLE_TAP;
-        else return TD_SINGLE_HOLD;
-    } else if (state->count == 2) {
-        if (!state->pressed) return TD_DOUBLE_TAP;
-        else return TD_DOUBLE_HOLD;
-    } else {
-        return TD_UNKNOWN;
-    }
-}
-
-static td_tap_t ql_tap_state = {
-    .state = TD_NONE
-};
-
-void num_fn_finished(tap_dance_state_t *state, void *user_data) {
-    ql_tap_state.state = cur_dance(state);
-    switch (ql_tap_state.state) {
-        case TD_SINGLE_TAP:
-            tap_code(KC_LEFT_GUI);
-            break;
-        case TD_SINGLE_HOLD:
-            layer_on(NUM_LAYER);
-            break;
-        case TD_DOUBLE_TAP:
-            if (layer_state_is(NUM_LAYER)) {
-                layer_off(NUM_LAYER);
-            }
-            break;
-        case TD_DOUBLE_HOLD:
-            if (layer_state_is(NUM_LAYER)) {
-                layer_off(NUM_LAYER);
-            } else {
-                layer_on(FN_LAYER);
-            }
-            break;
-        default:
-            break;
-    }
-}
-
-void num_fn_reset(tap_dance_state_t *state, void *user_data) {
-    if (ql_tap_state.state == TD_SINGLE_HOLD) {
-        layer_off(NUM_LAYER);
-    }
-    if (ql_tap_state.state == TD_DOUBLE_HOLD) {
-        layer_off(FN_LAYER);
-    }
-    ql_tap_state.state = TD_NONE;
-}
