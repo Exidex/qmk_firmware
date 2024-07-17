7c59050842472e55c7810c64f9d19b72994f9b49
Exidex <16986685+Exidex@users.noreply.github.com>
2024-07-17 21:55:43 +0200

    Migrate to one-shot mods. Remove combos

--- keyboards/beekeeb/piantor_pro/keymaps/default/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/default/keymap.c
@@ -16,21 +16,11 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     NULL // terminate the array
 };
 
-const uint16_t PROGMEM combo_cut[] = {KC_E, KC_R, COMBO_END};
-const uint16_t PROGMEM combo_copy[] = {KC_D, KC_F, COMBO_END};
-const uint16_t PROGMEM combo_paste[] = {KC_C, KC_V, COMBO_END};
-const uint16_t PROGMEM copy_undo[] = {KC_W, KC_E, COMBO_END};
-combo_t key_combos[] = {
-    COMBO(combo_copy, LCTL(KC_C)),
-    COMBO(combo_cut, LCTL(KC_X)),
-    COMBO(combo_paste, LCTL(KC_V)),
-    COMBO(copy_undo, LCTL(KC_Z)),
-};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEFAULT_LAYER] = LAYOUT_split_3x6_3(
         KC_TAB,      KC_Q,         KC_W,          KC_E,         KC_R,            KC_T,                               KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,            KC_RALT,
-        KC_ESCAPE,   LGUI_T(KC_A), LALT_T(KC_S),  LCTL_T(KC_D), LSFT_T(KC_F),    KC_G,                               KC_H,    RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_INS,
+        KC_ESCAPE,   KC_A,         KC_S,          KC_D,         KC_F,            KC_G,                               KC_H,    KC_J,         KC_K,         KC_L,         KC_SCLN,         KC_INS,
         KC_LSFT,     KC_Z,         KC_X,          KC_C,         KC_V,            KC_B,                               KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RSFT,
                                                 MO(SYMBOL_LAYER),   MO(NAV_LAYER),   BSPC_OR_DEL,                 KC_SPC, KC_ENT, MO(NUM_LAYER)
     ),
@@ -43,24 +33,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [SYMBOL_LAYER] = LAYOUT_split_3x6_3(
-        KC_NO,     KC_EXLM,    KC_AT,     KC_HASH,   KC_DLR,    KC_PERC,                         KC_CIRC,   KC_AMPR,  KC_ASTR,    KC_UNDS,    KC_PLUS,   KC_NO,
-        KC_NO,     KC_GRV,     KC_TILDE,  KC_LBRC,   KC_RBRC,   KC_BSLS,                         KC_QUOT,   KC_LPRN,  KC_RPRN,    KC_EQL,     KC_MINUS,  KC_NO,
-        KC_NO,     KC_NO,      KC_NO,     KC_LT,     KC_GT,     KC_PIPE,                         KC_DQUO,   KC_LCBR,  KC_RCBR,    KC_NO,      KC_NO,     KC_NO,
-                                                           KC_NO, KC_NO, KC_NO,           KC_NO, KC_NO, MO(FN_LAYER)
+        KC_NO,     KC_EXLM,         KC_AT,           KC_LBRC,        KC_RBRC,         KC_PERC,                     KC_CIRC,   KC_LPRN,  KC_RPRN,    KC_UNDS,    KC_PLUS,   KC_NO,
+        KC_NO,   OSM(MOD_LGUI),   OSM(MOD_LALT),   OSM(MOD_LCTL),  OSM(MOD_LSFT),   KC_BSLS,                     KC_QUOT,   KC_AMPR,  KC_ASTR,    KC_EQL,     KC_MINUS,  KC_NO,
+        KC_NO,      KC_HASH,         KC_DLR,          KC_LT,          KC_GT,           KC_PIPE,                     KC_DQUO,   KC_LCBR,  KC_RCBR,    KC_GRV,      KC_TILDE,     KC_NO,
+                                                                                 KC_NO, KC_NO, KC_NO,           KC_NO, KC_NO, MO(FN_LAYER)
     ),
 
     [NUM_LAYER] = LAYOUT_split_3x6_3(
-        KC_AUDIO_MUTE,        KC_NO,    KC_NO,    KC_NO,    KC_MEDIA_NEXT_TRACK,    KC_AUDIO_VOL_UP,                              KC_NO,    KC_7,    KC_8,    KC_9,    KC_NO,    KC_NO,
-        KC_MEDIA_STOP,        KC_NO,    KC_NO,    KC_NO,    KC_MEDIA_PREV_TRACK,    KC_AUDIO_VOL_DOWN,                            KC_0,     KC_4,    KC_5,    KC_6,    KC_NO,    KC_NO,
-        KC_MEDIA_PLAY_PAUSE,  KC_NO,    KC_NO,    KC_NO,    KC_NO,                  KC_NO,                                        KC_NO,    KC_1,    KC_2,    KC_3,    KC_NO,    KC_NO,
-                                                                                   MO(FN_LAYER), KC_NO, KC_NO,               KC_NO, KC_NO, KC_NO
+        KC_NO,        KC_NO,    KC_7,    KC_8,    KC_9,    KC_NO,                           KC_NO,    KC_NO,         KC_NO,          KC_NO,          KC_NO,           KC_NO,
+        KC_NO,        KC_NO,    KC_4,    KC_5,    KC_6,    KC_0,                            KC_NO,    OSM(MOD_RSFT), OSM(MOD_RCTL),  OSM(MOD_LALT),  OSM(MOD_RGUI),   KC_NO,
+        KC_NO,        KC_NO,    KC_1,    KC_2,    KC_3,    KC_NO,                           KC_NO,    KC_NO,         KC_NO,          KC_NO,          KC_NO,           KC_NO,
+                                                    MO(FN_LAYER), KC_NO, KC_NO,           KC_NO, KC_NO, KC_NO
     ),
 
     [FN_LAYER] = LAYOUT_split_3x6_3(
-        KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                               KC_F12,    KC_F7,    KC_F8,    KC_F9,    KC_NO,    QK_BOOT,
-        KC_NO,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  KC_NO,                               KC_F11,    KC_F4,    KC_F5,    KC_F6,    KC_NO,    KC_NO,
-        KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                               KC_F10,    KC_F1,    KC_F2,    KC_F3,    KC_NO,    KC_NO,
-                                                   KC_NO, KC_NO, KC_NO,            KC_NO, KC_NO, KC_NO
+        KC_NO,            KC_MEDIA_STOP,            KC_NO,            KC_NO,           KC_MEDIA_PREV_TRACK,    KC_MEDIA_NEXT_TRACK,          KC_F12,    KC_F7,    KC_F8,    KC_F9,    KC_NO,    QK_BOOT,
+        KC_AUDIO_MUTE,    OSM(MOD_LGUI),            OSM(MOD_LALT),    OSM(MOD_LCTL),   OSM(MOD_LSFT),          KC_NO,                        KC_F11,    KC_F4,    KC_F5,    KC_F6,    KC_NO,    KC_NO,
+        KC_NO,            KC_MEDIA_PLAY_PAUSE,      KC_NO,            KC_NO,           KC_AUDIO_VOL_DOWN,      KC_AUDIO_VOL_UP,              KC_F10,    KC_F1,    KC_F2,    KC_F3,    KC_NO,    KC_NO,
+                                                                                                             KC_NO, KC_NO, KC_NO,            KC_NO, KC_NO, KC_NO
     ),
 
 };
--- keyboards/beekeeb/piantor_pro/rules.mk
+++ keyboards/beekeeb/piantor_pro/rules.mk
@@ -1,4 +1,3 @@
 SERIAL_DRIVER = vendor
 
 KEY_OVERRIDE_ENABLE = yes
-COMBO_ENABLE = yes
