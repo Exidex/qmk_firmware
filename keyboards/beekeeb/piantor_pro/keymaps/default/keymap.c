6f66e6d775581b1617d4730353908f7b356a87a1
Exidex <16986685+Exidex@users.noreply.github.com>
2024-08-09 15:53:52 +0200

    Swap backspace and space

--- keyboards/beekeeb/piantor_pro/keymaps/default/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/default/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESCAPE,   KC_A,         KC_S,          KC_D,         KC_F,            KC_G,                               KC_H,    KC_J,         KC_K,         KC_L,         KC_SCLN,         KC_INS,
         KC_LSFT,     KC_Z,         KC_X,          KC_C,         KC_V,            KC_B,                               KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RSFT,
 
-        OSL_MOD_LAYER,   MO(NAV_LAYER),   BSPC_OR_DEL,                 KC_SPC, MO(SYMBOL_LAYER), KC_ENT
+        OSL_MOD_LAYER, MO(NAV_LAYER), KC_SPC,                 BSPC_OR_DEL, MO(SYMBOL_LAYER), KC_ENT
     ),
 
     [NAV_LAYER] = LAYOUT_split_3x6_3(
