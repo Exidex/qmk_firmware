595614c0b1d496331c1b3b20f768f773d42f4605
Exidex <16986685+exidex@users.noreply.github.com>
2025-02-23 11:42:37 +0100

    remove kc_del

--- keyboards/beekeeb/piantor_pro/keymaps/default/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/default/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,  KC_HOME,      KC_LEFT,         KC_DOWN,    KC_RIGHT,    KC_END,                                XXXXXXX,    XXXXXXX,       XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
         XXXXXXX,  XXXXXXX,      S(KC_TAB),       KC_APP,     KC_TAB,      KC_PGDN,                               XXXXXXX,    XXXXXXX,       XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
 
-        XXXXXXX, XXXXXXX, XXXXXXX,                KC_DEL, MO(FN_LAYER), XXXXXXX
+        XXXXXXX, XXXXXXX, XXXXXXX,                XXXXXXX, MO(FN_LAYER), XXXXXXX
     ),
 
     [NAV_LAYER_MACOS] = LAYOUT_split_3x6_3(
